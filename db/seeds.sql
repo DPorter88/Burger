019f762064369e10cb95a9085227872b85cceb2a
Dorian Porter <dporter88.dp@gmail.com>
2019-10-31 14:48:19 -0500

    added data to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Waffle Turkey Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Black Angus Bleu Cheese Burger');
