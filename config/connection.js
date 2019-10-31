2c850431ee1ced1ee20f66fb9abb0153a8aa15fe
Dorian Porter <dporter88.dp@gmail.com>
2019-10-31 13:26:19 -0500

    added connection info

--- config/connection.js
+++ config/connection.js
@@ -3,4 +3,22 @@ var mysql = require("mysql");
 var connection;
 
 
-connection = mysql.createConnection(
+connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password: "TDen!se31"
+    database: "burgers_db"
+
+});
+}
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+//MySqlConnection Exported
+  module.exports = connection;
