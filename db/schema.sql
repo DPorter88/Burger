20023f9b70f6867fb2b4ddd7e289e354add5d1bc
Dorian Porter <dporter88.dp@gmail.com>
2019-10-31 13:04:43 -0500

    added server info

--- db/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE burgers_db
-USE burgers_db
-CREATE TABLE burgers
-(
-    id 
-    burger_name
-    devoured
-)
--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-require('express');
+var express = require('express');
+var methodOverride = require("method-override");
+var router = require("./controllers/burgers_controller.js");
+var exphdbrs = require("express-handlebars");
+var path = require("path");
+
+// Express
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+app.use(methodOverride("_method"));
+app.use(bodyParser.json());
+app.use
+app.use
+app.use
+
